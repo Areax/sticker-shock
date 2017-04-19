ab37a38450dd43e6d1e77e0a7c2033120d653d6a
Dan Lott <danmanlott@gmail.com>
2017-04-19 16:05:48 -0400

    Add files via upload

--- application/controllers/account_controller.php
+++ application/controllers/account_controller.php
@@ -9,28 +9,15 @@ class Account extends Controller {
         # If user is logged in, fetch their page.
         # If they aren't redirect to login page.
         if (isset($_SESSION['username'])) {
-
-            //get account infomation
-            $info = $this->model->readUser($_SESSION['id']);
-            $_SESSION['accInfo'] = $info;
-
-            //get list of order
-            $orders = $this->model->getOrderFromUser($_SESSION['id']);
-            $_SESSION['orderHis'] = $orders;
-//            var_dump($orders);
-
-            //get list of item that user is selling and sold
-            $listings = $this->model->getSaleList($_SESSION['id']);
-            $_SESSION['listing'] = $listings;
-          
-            $user = $this->model->readUser($_SESSION['id']);
-            $orders = null;
             require 'application/models/Item.php';
+            require 'application/models/Order.php';
+            $order_helper = new Order($this->db);
+            $user = $this->model->readUser($_SESSION['id']);
+            $orders = $this->model->getOrderFromUser($_SESSION['id']);
+            $listings = $this->model->getSaleList($_SESSION['id']);
+            $items = new Item($this->db);
+            require 'application/models/Review.php';
             require 'application/views/account/index.php';
-//            unset($_SESSION['orderHis']);
-//            unset($_SESSION['accInfo']);
-//            unset($_SESSION['listing']);
-
         } else {
             header('location: /account/login');
         }
@@ -42,6 +29,8 @@ class Account extends Controller {
             $url = filter_var($url, FILTER_SANITIZE_URL);
             $url = explode(',',$url); 
         }
+        if(isset($_SERVER['HTTP_REFERER']))
+            $_SESSION['http_ref'] = $_SERVER['HTTP_REFERER'];
         $this->title = 'Log In';
         require 'application/views/account/login.php';
     }
@@ -72,23 +61,27 @@ class Account extends Controller {
 
     public function edit(){
         $this->title = 'Edit Account Information';
-
+        $user = $this->model->readUser($_SESSION['id']);
         require 'application/views/account/edit.php';
 
     }
     public function submit_edit(){
-
         $fname = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_STRING);
         $lname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING);
         $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-        $gender = filter_input(INPUT_POST, 'gender', FILTER_SANITIZE_STRING);
-        $address1 = filter_input(INPUT_POST, 'address1', FILTER_SANITIZE_STRING);
-        $address2 = filter_input(INPUT_POST, 'address2', FILTER_SANITIZE_STRING);
-        $city = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING);
-        $state = filter_input(INPUT_POST, 'state', FILTER_SANITIZE_STRING);
-        $zip = filter_input(INPUT_POST, 'zip', FILTER_SANITIZE_STRING);
-        $this->model->updateUser($_SESSION['id'],$fname,$lname,$email,$gender,$address1,$address2,$city,$state,$zip);
-        $this->index();
+        $paypal_email = filter_input(INPUT_POST, 'paypal_email', FILTER_SANITIZE_EMAIL);
+        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+        $confirm_password = filter_input(INPUT_POST, 'confirm_password', FILTER_SANITIZE_STRING);
+        $hashpass = password_hash($password, PASSWORD_DEFAULT);
+        $this->model->validateEdit($_SESSION['id'], $username, $email, $password, $confirm_password);
+        if((isset($_SESSION['username_taken_err']) && $_SESSION['username_taken_err'] != '') || (isset($_SESSION['email_taken_err']) && $_SESSION['email_taken_err'] != '') || (isset($_SESSION['pwd_match_err']) && $_SESSION['pwd_match_err'] != '')) {
+            $this->edit();
+        }
+        else {
+            $this->model->updateUser($_SESSION['id'], $fname, $lname, $username, $email, $password, $hashpass, $paypal_email);
+            $this->index();
+        }
     }
 
     public function submit_signup() {
@@ -96,20 +89,17 @@ class Account extends Controller {
         $fname = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_STRING);
         $lname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING);
         $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $paypal_email = filter_input(INPUT_POST, 'paypal_email', FILTER_SANITIZE_EMAIL);
         $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+        $confirm_password = filter_input(INPUT_POST, 'confirm_password', FILTER_SANITIZE_STRING);
         $hashpass = password_hash($password, PASSWORD_DEFAULT);
-        $gender = filter_input(INPUT_POST, 'gender', FILTER_SANITIZE_STRING);
-        $address1 = filter_input(INPUT_POST, 'address1', FILTER_SANITIZE_STRING);
-        $address2 = filter_input(INPUT_POST, 'address2', FILTER_SANITIZE_STRING);
-        $city = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING);
-        $state = filter_input(INPUT_POST, 'state', FILTER_SANITIZE_STRING);
-        $zip = filter_input(INPUT_POST, 'zip', FILTER_SANITIZE_STRING);
-        $this->model->validateRegistration($username, $email);
-        if((isset($_SESSION['username_taken_err']) && $_SESSION['username_taken_err'] != '') || (isset($_SESSION['email_taken_err']) && $_SESSION['email_taken_err'] != '') ) {
+        $this->model->validateRegistration($username, $email, $password, $confirm_password);
+        if((isset($_SESSION['username_taken_err']) && $_SESSION['username_taken_err'] != '') || (isset($_SESSION['email_taken_err']) && $_SESSION['email_taken_err'] != '') || (isset($_SESSION['pwd_match_err']) && $_SESSION['pwd_match_err'] != '')) {
+            $_SESSION['POST'] = $_POST;
             $this->signup();
         }
         else {
-            $this->model->createUser($username, $fname, $lname, $email, $hashpass, $gender, $address1, $address2, $city, $state, $zip);
+            $this->model->createUser($username, $fname, $lname, $email, $hashpass, $paypal_email);
             $this->login();
         }
     }
@@ -131,55 +121,52 @@ class Account extends Controller {
         }
     }
 
-    public function otherAccount() {
-        $user_id = filter_input(INPUT_POST, 'user', FILTER_SANITIZE_STRING);
-        //require 'application/models/User.php';
-        $user_model = new User($this->db);
-        $user = $user_model->readUser($user_id);
-        $listings = $user_model->getItemsByUser($user_id);
+    public function profile($user_id) {
         require 'application/models/Review.php';
         require 'application/models/Order.php';
-        require 'application/views/account/otherAccount.php';
+        $users = $this->model;
+        $user = $this->model->readUser($user_id);
+        $listings = $this->model->getItemsByUser($user_id);
+        $review = new Review($this->db);
+        $reviews = $review->getReviewsByUser($user->user_id);
+        $this->title = $user->username . '\'s Account History';
+        require 'application/views/account/profile.php';
+
+    }
+
+
+    public function writeReview($orderId){
+
+
+    }
+    public function deleteReview($reviewId){
+        require 'application/models/Review.php';
+        $review = new Review($this->db);
+        $review->deleteReview($reviewId);
+        header('location: /account/');
+
+    }
+
+    public function invoice($orderId){
+        if(!$orderId)
+            require 'application/views/pages/error.php';
+        else {
+            require 'application/models/Order.php';
+            require 'application/models/Item.php';
+            $this->title = 'Invoice';
+            $invoice = $this->model->getPurchase($orderId);
+            $order_helper = new Order($this->db);
+            $details = $order_helper ->getItemIdFromOrderId($orderId);
+            $item_helper = new Item($this->db);
+            $item = $item_helper->getItemById($details->item_id);
+            require 'application/views/account/invoice.php';
+
+        }
     }
 
     public function loadModel() {
         require 'application/models/User.php';
         $this->model = new User($this->db);
         return;
-    }
-
-    public function viewOrder($account_id){
-        $this->title = 'View Previous Orders';
-//        diplay all order in one page
-        $orders = $this->model->getOrderFromUser($account_id);
-        $_SESSION['orderHis'] = $orders;
-
-        require 'application/views/account/vieworder.php';
-        unset($_SESSION['orderHis']);
-    }
-    public function viewListing($user_id){
-        $this->title = 'View All Sale';
-//        display in one page
-        $listing = $this->model->getSaleList($user_id);
-        $_SESSION['listing'] = $listing;
-
-        require 'application/views/account/viewListing.php';
-    }
-
-    public function writeReview($orderId){
-
-
-    }
-    public function deleteReview($orderId){
-
-    }
-    public function printInvoice($orderId){
-        if(!$orderId)
-            require 'application/views/pages/error.php';
-        else {
-            $_SESSION['invoice'] = $this->model->getPurchase($orderId);
-            require 'application/views/account/printInvoice.php';
-            unset($_SESSION['invoice']);
-        }
     }
 }
