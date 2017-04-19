3c8c490d4023f888c4ff7b1f5221b1b151946303
Dan Lott <danmanlott@gmail.com>
2017-04-19 16:08:19 -0400

    Add files via upload

--- application/views/items/item.php
+++ application/views/items/item.php
@@ -3,44 +3,72 @@
         <div class="row">
             <?php include 'application/views/includes/category-menu.php';?>
             <div class="col-md-9">
-                <div class="card">
-                    <img src="<?php if(file_exists('uploads/item_'. $item->item_id)) {echo '/uploads/item_'.$item->item_id;} else {echo 'https://placehold.it/800x300';}?>" alt="">
+                <div class="card break-word">
+                    <img class="card-img-top img-fluid" style="object-fit: contain;" src="<?php if(file_exists('uploads/item_'. $item->item_id)) {echo '/uploads/item_'.$item->item_id;} else {echo 'https://placehold.it/800x300';}?>" alt="">
                     <div class="card-block">
                         <h4 style="display:inline-block;"><?php echo $item->item_name?></h4>
                         <h5 class="float-right" style="display:inline-block;">$<?php echo number_format((float)$item->price, 2, '.', ''); ?></h5>
+                        <br>
+                        <small class="float-right" style="display:inline-block;">+ $<?php echo number_format((float)$item->shipping, 2, '.', '') . ' S&H' ?></small>
+                        </small>
                         <p><?php echo $item->description?></p>
                         <div class="text-left">
+                            <!-- this is only a form because paypal is picky -->
                             <form action="/items/purchaseitem" method="POST">
                                 <input type="hidden" name="item_name" value="<?php echo $item->item_name?>">
                                 <input type="hidden" name="price" value="<?php echo $item->price?>">
                                 <input type="hidden" name="shipping" value="<?php echo $item->shipping?>">
                                 <input type="hidden" name="seller_id" value="<?php echo$item->account_id?>">
                                 <input type="hidden" name="item_id" value="<?php echo $item->item_id?>">
+                                <?php if((isset($_SESSION['id']) && $_SESSION['id'] != $item->account_id) || !isset($_SESSION['id'])){?>
                                 <button type="submit" class="btn-ss btn-bw" name="submit"> Purchase
                                 </button>
+                                <?php }?>
                             </form>
                         </div>
                         <br>
-                        <h5><a href="/accountdummy"> Account Username: <?php echo $item->account_id;?></a></h5>
                     </div>
                     <div class="card-footer">
                         <big>
-                            <?php
-                            if(isset($item->rating)){
-                                $avgReview=$item->rating;
-                            }
-                            else{
-                                $avgReview = 0;
-                            }
-                            for ($i=0; $i<5; $i++) {
-                                if ($avgReview - $i >= 0.5)
-                                    echo '&#9733; ';
-                                else
-                                    echo '&#9734; ';
-                            }?>
+                            <small>
+                                <a href="/account/profile/<?php echo $user->readUser($item->account_id)->user_id;?>">
+                                    <?php echo $user->readUser($item->account_id)->username;?>
+                                </a>
+                            </small>
+                            <div class="float-right">
+                                <?php
+                                if(isset($user->readUser($item->account_id)->rating)){
+                                    $avgReview= $user->readUser($item->account_id)->rating;
+                                }
+                                else{
+                                    $avgReview = 0;
+                                }
+                                for ($i=0; $i<5; $i++) {
+                                    if ($avgReview - $i >= 0.5)
+                                        echo '&#9733; ';
+                                    else
+                                        echo '&#9734; ';
+                                }
+                                ?>
+                            </div>
                         </big>
                     </div>
                 </div>
+<?php 
+    if (sizeof($reviews) > 0) {
+        echo'<br>';
+        echo '<div class="well">';
+        include "application/views/items/reviews.php";
+        echo '</div>';
+    }
+
+    else{
+        echo'<br>';
+        echo '<div class="well">';
+        echo '<p>This user has no reviews.</p>';
+        echo '</div>';
+    }
+?>
             </div>
         </div>
     </div>
--- application/views/items/review.php
+++ application/views/items/review.php
@@ -4,34 +4,41 @@
     <div class="container">
         <div class="h1">Submit a Review<hr></div>
         <form action="/reviews/submit_review" method="POST">
+            <input type="hidden" name="orderID" value='<?php
+            $oID = $_SESSION['orderID'];
+            echo $oID;
+            unset($_SESSION['orderID']); ?>' />
+            <div class="form-group row">
+                <div class="col-md-6">
+                    <input required type="text" class="form-control" name="title" placeholder="Title">
+                </div>
+            </div>
             <div class="form-group row">
                 <div class="col-md-6">
                     <select class="form-control" name="rating">
-                        <option value="" disabled selected>Select Stars to give</option>
-                        <option value="1">&#9733;</option>
-                        <option value="2">&#9733;&#9733;</option>
-                        <option value="3">&#9733;&#9733;&#9733;</option>
-                        <option value="4">&#9733;&#9733;&#9733;&#9733;</option>
-                        <option value="5">&#9733;&#9733;&#9733;&#9733;&#9733;</option>
+                        <option value="" disabled selected>Rating</option>
+                        <option value="1">1 (Worst)</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5 (Best)</option>
                     </select>
                 </div>
             </div>
             <div class="form-group row">
                 <div class="col-md-6">
-                    <input required type="text" class="form-control" name="title" placeholder="Title" rows="3">
+                    <textarea placeholder="Description" class="form-control" name="comment" rows="6"></textarea>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-md-6"><div class="form-group">
-                    <textarea class="form-control" rows="3" name="comment" placeholder="Description"></textarea>
-                </div></div>
             </div>
 
             <input type="hidden" name="sellerID" value='<?php echo $sellerID; ?>' />
+            <input type="hidden" name="orderID" value='<?php echo $oID; ?>' />
 
 
-            <div class="form-group">
-                <button type="submit" class="btn-ss btn-bw"" name="submit">Submit</button>
+            <div class="form-group row">
+                <div class="col-md-6">
+                    <button type="submit" class="btn-ss btn-bw"" name="submit">Submit</button>
+                </div>
             </div>
 
 
